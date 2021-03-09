acfaf0f94fa8635b5572e07ecb88b5d3ca8533f3
SakshiPatmase <SakshiPatmase>
2021-03-09 18:46:30 +0530

    java file uploaded

--- src/main/java/com/demo/Calculator.java
+++ src/main/java/com/demo/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 	static public int add(int... nums) {
 		int result = 0;
 		for(int num:nums)
-			result += num;
-		return result;
+			result += num
+		return result
 	}
 }
